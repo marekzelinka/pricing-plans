54941e9e522632068381023d6ca21bd7d100aa41
Marek Zelinka <mzelinka17@gmail.com>
2024-12-10 11:02:09 +0100

    base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export default function PricingPlans() {
-  return <></>;
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-4xl text-center">
+          <h1 className="text-base/7 font-semibold text-indigo-600">
+            Pricing plans
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
